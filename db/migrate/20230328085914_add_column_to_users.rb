d6b71aa341bee06e617d82fe66c806550c5a92b6
Ralambotsirofo Mathieu <m.ralambotsirofo@gmail.com>
2023-03-28 11:27:40 +0200

    made a simple db with only 3 tables

---
+++ db/migrate/20230328085914_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :sport, :string
+    add_column :users, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_085012) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_092556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "friendships", force: :cascade do |t|
-    t.bigint "asker_id"
-    t.bigint "receiver_id"
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["asker_id"], name: "index_friendships_on_asker_id"
-    t.index ["receiver_id"], name: "index_friendships_on_receiver_id"
+    t.index ["room_id"], name: "index_bookings_on_room_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "sport"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,10 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_085012) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "sport"
+    t.string "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "friendships", "users", column: "asker_id"
-  add_foreign_key "friendships", "users", column: "receiver_id"
+  add_foreign_key "bookings", "rooms"
+  add_foreign_key "bookings", "users"
 end
