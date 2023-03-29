d764061193f7470a55dff09ad7766766603f4862
Ralambotsirofo Mathieu <m.ralambotsirofo@gmail.com>
2023-03-29 15:28:24 +0200

    flatpickr done with some disable date

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,12 +1,31 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
-// Connects to data-controller="flatpickr"
 export default class extends Controller {
   static targets = ["startDate", "endDate"];
 
   connect() {
-    flatpickr(this.startDateTarget, {});
-    flatpickr(this.endDateTarget, {});
+    flatpickr(this.startDateTarget, {
+      minDate: "today",
+      dateFormat: "Y-m-d",
+      disable: [
+        {
+          // Désactiver toutes les dates antérieures à aujourd'hui
+          from: new Date().setHours(0, 0, 0, 0),
+          to: new Date(0),
+        },
+      ],
+    });
+    flatpickr(this.endDateTarget, {
+      minDate: "today",
+      dateFormat: "Y-m-d",
+      disable: [
+        {
+          // Désactiver toutes les dates antérieures à aujourd'hui
+          from: new Date().setHours(0, 0, 0, 0),
+          to: new Date(0),
+        },
+      ],
+    });
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :rooms
+  resources :rooms do
+    resources :bookings, only: [:create]
+  end
+
+
+  patch "booking/:id/accepted", to: "bookings#accepted", as: :booking_accepted
+  patch "booking/:id/refused", to: "bookings#refused", as: :booking_refused
+
 end
